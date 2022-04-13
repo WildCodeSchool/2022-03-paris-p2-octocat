391d65adfaf544dae999a1bbaa251ab991b899cd
Gregoria R <rubio.gregoria@gmail.com>
2022-04-13 20:30:32 +0200

    finished banner for desktop and mobile

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../components/homepage/Banner';
+
+
+const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <Banner />
+    </>
+  )
+}
+
+export default HomePage
