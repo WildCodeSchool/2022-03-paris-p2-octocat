8a606be4c870d289f2be843557b57ee94a86ad4e
Gregoria R <rubio.gregoria@gmail.com>
2022-04-14 15:08:32 +0200

    added some css to moviecard

--- src/components/homepage/MovieSlider.jsx
+++ src/components/homepage/MovieSlider.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard'
 function MovieSlider() {
   return (
     <>
-      <div className='flex justify-between'>
+      <div className='flex justify-evenly h-40'>
         <MovieCard />
         <MovieCard />
         <MovieCard />
