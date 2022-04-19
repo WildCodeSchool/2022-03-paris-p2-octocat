873a7ade02b8616726f4745ac265aff6c8319fb1
Gregoria R <rubio.gregoria@gmail.com>
2022-04-19 21:52:33 +0200

    created component and added route + link on banner button "more info"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import HomePage from './pages/HomePage';
 import { Routes, Route } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
     <div className="App text-white font-Heebo bg-black">
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
 
     </div>
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,9 @@
+
+
+function MovieDetails() {
+  return (
+    <div>MovieDetails</div>
+  )
+}
+
+export default MovieDetails
