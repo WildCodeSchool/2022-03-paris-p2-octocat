b009f989f4f0a1da35c0d041051f35385dadfc81
Gregoria R <rubio.gregoria@gmail.com>
2022-04-19 20:28:20 +0200

    added request to api to display movies genres and tv shows titles

--- src/components/homepage/MovieSlider.jsx
+++ src/components/homepage/MovieSlider.jsx
@@ -4,8 +4,9 @@ const axios = require('axios');
 
 function MovieSlider({title, dataSource}) {
 
-  const [movieList, setMovieList] = useState([]);
+  // fetching all data from lists based on URLs defined in HomePage
 
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     axios.get(dataSource)
@@ -17,8 +18,33 @@ function MovieSlider({title, dataSource}) {
     })
   },[]);
 
-  console.log("movielist", movieList)
+  // fetching list of movies genres
 
+  const [genres, setGenres] = useState([]);
+  const getGenres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=0aba5a6d503daa5780b386d6fd32a451';
+
+  useEffect(() => {
+    axios.get(getGenres)
+    .catch(function (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    })
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      setGenres(data);
+    })
+
+  },[]);
 
   return (
     <>
@@ -26,16 +52,14 @@ function MovieSlider({title, dataSource}) {
         <div className='font-extrabold text-xl pl-4 py-4'>
           <h2>{title}</h2>
         </div>
-        <div className='flex h-full overflow-x-auto overflow-y-hidden'>
+        <div className='flex h-full overflow-x-scroll overflow-y-hidden'>
           {movieList.length > 0 &&
             movieList.map((movie,i) => {
               return (            
-                <MovieCard key={movie.id} data={movie}/>
+                <MovieCard key={movie.id} data={movie} genres={genres}/>
               )
             })
           }
-
-
         </div>
       </div>
     </>
