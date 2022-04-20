1aff28777967b67ff5e2849401e2149af043d609
Hello-Wild <briac-rousseau_student2022@wilder.school>
2022-04-20 17:29:12 +0200

    searchbar styling done

--- src/components/homepage/SearchBar.jsx
+++ src/components/homepage/SearchBar.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
+const searchIcon = <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+</svg>
+
 const SearchBar = () => {
 
   const [searchQuery, setSearchQuery] = useState('');
@@ -23,13 +27,14 @@ const SearchBar = () => {
 
   return (
     <div className="flex items-center text-black" id="search-bar">
-      <div id="search-bar-container">
+      <div id="search-bar-container" className="flex items-center gap-4">
         <div id="search-bar-input">
-          <input id= "coucou" type="text" placeholder="Search for a movie or a TV show"
-           value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
+          <input id= "research-text-field" className="flex rounded-lg py-1 px-2 border-transparent" 
+            type="text" placeholder="Enter a movie title..."
+            value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
         </div>
-        <div id="search-bar-button">
-            <button className="text-white" disabled={!searchQuery} onClick={handleNavigate}>coucou</button>
+        <div id="search-bar-button" className="flex justify-center items-center">
+            <button className="text-white" disabled={!searchQuery} onClick={handleNavigate}>{searchIcon}</button>
         </div>
         <div>
           <ul id="search-results">
