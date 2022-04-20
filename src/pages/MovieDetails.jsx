878dc711ecd04350f1b7089a66540a0af8ccdf63
Gregoria R <rubio.gregoria@gmail.com>
2022-04-20 11:44:41 +0200

    finished css for desktop version

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,14 +5,19 @@ function MovieDetails() {
   return (
     <>
       <Navbar />
-        <div className="h-screen py-4 px-4 sm:pl-12 sm:pr-12">
-          <div className="sm:w-screen sm:flex sm:justify-center">
+        <div className="h-screen py-4 sm:pr-12">
+          <div className="relative w-full overflow-hidden text-center sm:w-screen sm:flex sm:justify-center ">
               <img alt='poster' src='https://picsum.photos/1200/480'></img>
+              <div className="absolute inset-y-1/4">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-40 w-40 opacity-50" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
+                </svg>
+              </div>
           </div>
-          <div className="sm:flex sm:justify-between sm:w-full py-10">
-            <div className="sm:w-2/4">
-              <h1 className="font-bold text-center pt-20 text-3xl sm:text-7xl sm:text-left">Movie title</h1>
-              <div className="flex">
+          <div className="sm:flex sm:items-start sm:justify-between sm:w-full pl-8 py-12">
+            <div className="flex flex-col justify-center sm:w-2/4 pl-20">
+              <h1 className="font-bold text-center text-3xl pb-2 sm:text-7xl sm:text-left">Movie title</h1>
+              <div className="flex pb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
                 </svg>
@@ -29,10 +34,10 @@ function MovieDetails() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
                 </svg>
               </div>
-              <div className="py-4 sm:w-4/5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
+              <div className="py-4 sm:w-full">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</div>
             </div>
-            <div className="flex flex-col justify-center items-center sm:w-2/5">
-              <div className="rounded-md shadow">
+            <div className="flex flex-col justify-start items-start pr-20 sm:w-2/5">
+              <div className="rounded-md shadow pb-10">
                 <button
                   className="flex justify-center items-center px-6 py-2 border-2 text-base font-medium rounded-md text-white bg-transparent transition duration-500 ease-in-out w-full sm:hover:scale-125"
                 >
@@ -42,23 +47,27 @@ function MovieDetails() {
                 </svg>
                 </button>
               </div>
-              <div>Type: movie or tv show</div>
-              <div>Release date</div>
-              <div>Movie genre</div>
-              <div>Country</div>
-              <div>Available languages</div>
-              <button>IMDB link</button>
+              <div className="flex justify-start items-start pb-10 sm:w-full">
+                <button className="bg-stone-700 px-2 py-2 mr-3 opacity-70">Action</button>
+                <button className="bg-stone-700 px-2 py-2 opacity-70">Aventure</button>
+              </div>
+              <div className="grid gap-2 grid-cols-2 grid-rows-4">
+                <div className="font-semibold">Release date :</div>
+                <div>15-07-2010</div>
+                <div className="font-semibold">Country :</div>
+                <div>United Kingdom, United States of America</div>
+                <div className="font-semibold">Original language :</div>
+                <div>EN FR</div>
+                <button className="font-semibold flex justify-start">
+                  <span className="bg-amber-400 w-12 h-6 text-neutral-900 font-black">IMDB</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 pl-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
+                  </svg>
+                </button>      
             </div>
           </div>
-          <div>You may like...</div>
-            <ul className="sm:flex">
-              <li>Movie</li>
-              <li>Movie</li>
-              <li>Movie</li>
-            </ul>
         </div>
-
-
+        </div>
       <Footer />
     </>
   )
