c63e933268f55aecc9735d0cf88db25ca9814e14
Gregoria R <rubio.gregoria@gmail.com>
2022-04-21 14:26:53 +0200

    ready to merge with dev

--- src/components/homepage/MovieCard.jsx
+++ src/components/homepage/MovieCard.jsx
@@ -12,7 +12,7 @@ function MovieCard({data, moviesGenres}) {
       return Object.getOwnPropertyNames(data).includes("first_air_date") ? setIsTv(true) : setIsTv(false);
     }
     checkType();
-  },[]);
+  },[data]);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -25,8 +25,8 @@ function MovieCard({data, moviesGenres}) {
   
   return (
     <>
-      <div onClick={handleClick} className='z-10 relative rounded-md overflow-hidden flex justify-start items-end shrink-0 mr-8 w-80 h-40 bg-no-repeat bg-cover sm:hover:scale-110 sm:ease-in-out sm:duration-300' style={{backgroundImage:`url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`}}>
-        <div className='absolute w-full h-full pt-20 sm:pl-4'>
+      <div onClick={handleClick} className='cursor-pointer z-10 relative rounded-md overflow-hidden flex justify-start items-end shrink-0 mr-8 w-80 h-40 bg-no-repeat bg-cover sm:hover:scale-110 sm:ease-in-out sm:duration-300' style={{backgroundImage:`url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`}}>
+        <div className='absolute w-full h-full pt-12 pl-4'>
           <div className='card icons flex justify-between items-center w-1/4'>
             <button>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 hover:opacity-90 " viewBox="0 0 20 20" fill="currentColor">
