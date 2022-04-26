2896d420f57865e1d7ed7018419f44fc8a2d9c5b
Hello-Wild <briac-rousseau_student2022@wilder.school>
2022-04-26 14:45:05 +0200

    just want to move to dev branch damn it !

--- src/components/homepage/SearchBar.jsx
+++ src/components/homepage/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const searchIcon = <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
