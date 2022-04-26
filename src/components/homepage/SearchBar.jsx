174da989b1e888d7ba52a442684f1d0bfa3fe810
Hello-Wild <briac-rousseau_student2022@wilder.school>
2022-04-26 16:13:14 +0200

    add 'press enter to submit' feature to searchbar

--- src/components/homepage/SearchBar.jsx
+++ src/components/homepage/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const searchIcon = <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -16,12 +16,26 @@ const SearchBar = () => {
     navigate("/search-result",{state: {searchQuery}});
   }
 
+  // Press enter to search
+  const handleSearch = () => {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") { handleNavigate();}
+      })}
+
+      useEffect(() => {
+        const handleSearch = () => {
+          window.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") { handleNavigate();}
+          })}
+      },[]);
+
   return (
     <div className="flex items-center text-black" id="search-bar">
       <div id="search-bar-container" className="flex items-center gap-4">
         <div id="search-bar-input">
-          <input id= "research-text-field" className="flex rounded-lg py-1 px-2 border-transparent" 
-            type="submit" placeholder="Enter a movie title..."
+            <input id= "research-text-field" className="flex rounded-lg py-1 px-2 border-transparent" 
+            type="text" placeholder="Enter a movie title..." 
+            onKeyDown={handleSearch}
             value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
         </div>
         <div id="search-bar-button" className="flex justify-center items-center">
