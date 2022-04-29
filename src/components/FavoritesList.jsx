9675d39797cddcf47cfaefe8aca6ef680a20b763
Gregoria R <rubio.gregoria@gmail.com>
2022-04-29 11:51:06 +0200

    implemented the delete feature on favorites items

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -1,14 +1,19 @@
 import FavoriteItem from './FavoriteItem';
+import { useCallback, useState } from 'react';
 
 function FavoritesList() {
 
-  const favorites = JSON.parse(localStorage.getItem('favorites'));
+  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites')));
+
+  const removeItem = useCallback((movieId)  => {
+    setFavorites(favorites.filter((movie) => movie.id !== movieId));
+  }, [favorites]);
 
   return favorites === null ? "No favorites found !" : (
-    <div className='flex sm:flex-wrap sm:gap-x-10 sm:gap-y-10 h-screen'>
+    <div className='flex sm:flex-wrap sm:gap-x-10 sm:gap-y-10 h-screen pb-32'>
       {favorites.map((favorite) => 
         {
-        return <FavoriteItem movieId={favorite.id} />
+        return <FavoriteItem movieId={favorite.id} removeItem={removeItem} />
         }
       )}
     </div>
