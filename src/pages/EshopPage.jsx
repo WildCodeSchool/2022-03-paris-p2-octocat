b96e7ada4c39b5777a23385600569645eced10e4
Gregoria R <rubio.gregoria@gmail.com>
2022-05-08 19:48:32 +0200

    added cart button

--- src/pages/EshopPage.jsx
+++ src/pages/EshopPage.jsx
@@ -9,7 +9,9 @@ function EshopPage() {
     <>
     <Navbar/>
     <div className='py-4 px-4 w-screen text-gray-50 sm:px-14'>
-      <h2 className="text-3xl pb-4 font-bold text-red-600">Fakeflix shop</h2>
+      <div className='flex justify-between'>
+        <h2 className="text-3xl pb-4 font-bold text-red-600">Fakeflix shop</h2>
+      </div>
       <h3 className="text-xl font-normal pb-10">Order something before your movie starts !</h3>
       <ProductsView />
     </div>
